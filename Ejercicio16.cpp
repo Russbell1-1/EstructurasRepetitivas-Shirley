c4bc47ddbf04723c393f5f6f1f19e902e88da2b8
ShirleyCauna <caunashirley11@gmail.com>
2025-05-29 12:50:02 -0500

    Agrega ejercicio 16 de estructuras repetitivas

---
+++ Ejercicio16.cpp
@@ -0,0 +1,22 @@
+// 16. Diseñar un algoritmo que genere los primeros "n" términos de la serie de Fibonacci, donde cada número es la suma de los dos anteriores, comenzando con 0 y 1. Ejemplo: 0, 1, 1,2,3,5,8,13, …….
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a = 0, b = 1, c;
+
+    cout << "Ingrese cantidad de terminos: ";
+    cin >> n;
+
+    cout << "Serie Fibonacci: ";
+    for (int i = 0; i < n; i++) {
+        cout << a << " ";
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    cout << endl;
+    return 0;
+}
